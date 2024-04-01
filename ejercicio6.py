16c186c2ce8b4e0a0d3dd888f2e84e144425f02f
Isabellaramirez <150964832+Isabellaramirez@users.noreply.github.com>
2024-04-01 11:20:13 -0500

    Create ejercicio6.py

---
+++ ejercicio6.py
@@ -0,0 +1,5 @@
+numero=int(input("ingrese el numero para sacar el 30%, 60% y 90%"))
+primerporcentaje= (numero*30)/100
+segundoporcentaje= (numero*60)/100
+tercerporcentaje= (numero*90)/100
+print("los porcentajes son:", primerporcentaje, segundoporcentaje, tercerporcentaje, sep=" , ")
