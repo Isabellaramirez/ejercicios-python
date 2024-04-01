ee6b6dcc7392600bbd7a87348f47ee93466da3a8
Isabellaramirez <150964832+Isabellaramirez@users.noreply.github.com>
2024-04-01 11:18:19 -0500

    Create ejercicio4.py

---
+++ ejercicio4.py
@@ -0,0 +1,4 @@
+añoactual=int(input("ingrese el año actual SIN PUNTOS NI COMAS"))
+añodenacimiento=int(input("ingrese el año en que nacio SIN PUNTOS NI COMAS"))
+edad= añoactual-añodenacimiento
+print("su edad es:", edad)
