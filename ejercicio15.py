bd7ceaa7bca58b1baf194329b9d093a44bfa4e71
Isabellaramirez <150964832+Isabellaramirez@users.noreply.github.com>
2024-04-01 11:22:16 -0500

    Create ejercicio15.py

---
+++ ejercicio15.py
@@ -0,0 +1,8 @@
+numero=int(input("ingrese el numero"))
+mitad=numero/2
+if mitad == 100:
+    print("la mitad es igual a 100")
+    if mitad>100:
+     print("la mitad es mayor a 100")
+else:
+    print("la mitad es menor a 100")
